02c5574bc871902725c08ec40181fe851f1231b6
JoaoGFreire <joaogfff@gmail.com>
2024-11-13 11:50:00 -0500

    setup unity again

---
+++ Platformer project true/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum FacingDirection
+    {
+        left, right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
+        //manage the actual movement of the character.
+        Vector2 playerInput = new Vector2();
+        MovementUpdate(playerInput);
+    }
+
+    private void MovementUpdate(Vector2 playerInput)
+    {
+
+    }
+
+    //acceleration
+    //velocity
+    //is velocity greater than 0
+
+    public bool IsWalking()
+    {
+        return false;
+    }
+    public bool IsGrounded()
+    {
+        return false;
+    }
+
+    public FacingDirection GetFacingDirection()
+    {
+        return FacingDirection.left;
+    }
+}
